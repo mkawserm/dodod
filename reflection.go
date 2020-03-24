b2500ef666d1c94c39e14d2eced48588640107fa
kawser <mkawserm@gmail.com>
2020-03-24 19:55:40 +0600

    REFLECTION UPDATED

--- reflection.go
+++ reflection.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-func ExtractFields(document interface{}) map[string]string {
+func ExtractFields(document Document) map[string]string {
 	data := make(map[string]string)
 	extractFields(reflect.TypeOf(document), data)
 	return data
@@ -36,7 +36,7 @@ func extractFields(t reflect.Type, data map[string]string) {
 }
 
 func GetId(document Document) string {
-	return document.GetId()
+	return getId(reflect.TypeOf(document), reflect.ValueOf(document))
 }
 
 func GetType(document Document) string {
@@ -47,27 +47,27 @@ func GetType(document Document) string {
 //	return getDododType(reflect.TypeOf(document), reflect.ValueOf(document))
 //}
 
-//func getId(t reflect.Type, v reflect.Value) string {
-//	if t.Kind() == reflect.Ptr {
-//		return getId(t.Elem(), v.Elem())
-//	} else if t.Kind() == reflect.Struct {
-//		for i := 0; i < t.NumField(); i++ {
-//			f := t.Field(i)
-//			tags := strings.Split(f.Tag.Get("json"),",")
-//			if len(tags) > 0 {
-//				name := strings.TrimSpace(tags[0])
-//				if name == "id" {
-//					v2 := v.FieldByName(f.Name)
-//					if v2.Kind() == reflect.String {
-//						return v2.String()
-//					}
-//				}
-//			}
-//		}
-//	}
-//
-//	return ""
-//}
+func getId(t reflect.Type, v reflect.Value) string {
+	if t.Kind() == reflect.Ptr {
+		return getId(t.Elem(), v.Elem())
+	} else if t.Kind() == reflect.Struct {
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			tags := strings.Split(f.Tag.Get("json"), ",")
+			if len(tags) > 0 {
+				name := strings.TrimSpace(tags[0])
+				if name == "id" {
+					v2 := v.FieldByName(f.Name)
+					if v2.Kind() == reflect.String {
+						return v2.String()
+					}
+				}
+			}
+		}
+	}
+
+	return ""
+}
 
 //func getDododType(t reflect.Type, v reflect.Value) string {
 //	if t.Kind() == reflect.Ptr {
